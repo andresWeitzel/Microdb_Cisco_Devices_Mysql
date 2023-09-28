cf029676930b4c41364149f622fa96733f3a514b
Andrés Weitzel <andres96energy@hotmail.com>
2023-09-28 20:33:37 -0300

    Added update functions

--- Scripts/03_microdb_cisco_devices_DML_UPDATES.sql
+++ Scripts/03_microdb_cisco_devices_DML_UPDATES.sql
@@ -9,6 +9,10 @@
 -- DATABASE
 use microdb_cisco_devices;
 
+-- VARS
+SET @created_at = now();
+SET @updated_at = now();
+
 
 -- ---------------------------------------------------------------------------
 -- ---------------------------------------------------------------------------
@@ -22,21 +26,53 @@ describe rate_plans ;
 -- Change name and descriptions for type plan name
 update rate_plans set name = 'QERT-789-A'
 , description = 'plan A for individual monthly subscriptions'
+, update_date = @updated_at
 where id = 1 and name = 'QERT-789';
 
 update rate_plans set name = 'QERT-789-B'
 , description = 'plan B for individual annual subscriptions'
+, update_date = @updated_at
 where id = 2 and name = 'QERT-790';
 
 
 -- Change version_id and version_plan 
 update rate_plans set version_id = '777278'
 , version_plan = '2.7.1'
+, update_date = @updated_at
 where version_id = '777212';
 
 update rate_plans set version_id = '7772709'
 , version_plan = '1.4.6'
+, update_date = @updated_at
 where version_id = '777213';
+
+-- Change type_plan, subs_charge and tiers
+update rate_plans set type_plan = 'Monthly - Individual Subscriber Plan'
+, subscription_charge = 2.88
+, number_of_tiers = 1
+, update_date = @updated_at
+where name = 'QERT-789-A';
+
+select * from rate_plans;
+describe rate_plans ;
+
+-- ---------------------------------------------------------------------------
+-- ---------------------------------------------------------------------------
+
+
+-- ======= Tabla devices ===========
+
+select * from devices;
+describe devices;
+
+
+-- Update all fields
+update devices set iccid= '8988216716970004972'
+, status = 'ACTIVATION_READY'
+, rate_plan_id = 4
+, creation_date = @created_at
+, update_date = @updated_at
+where id=1;
 
 
 -- ---------------------------------------------------------------------------
@@ -48,3 +84,11 @@ where version_id = '777213';
 select * from devices_details;
 describe devices_details ;
 
+-- Update three first fields
+update devices_details as dev_det inner join devices as dev
+on dev_det.device_id = dev.id
+set imsi = '901161697004977'
+, msisdn = '778239013883123'
+, imei = '662712771627362'
+, dev_det .update_date = @updated_at
+where dev.id = 1;
